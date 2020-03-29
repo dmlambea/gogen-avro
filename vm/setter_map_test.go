701be0bb7b98b5a58aa3184f69c7abfb70053cf5
Daniel M. Lambea <dmlambea@gmail.com>
2020-03-29 20:33:02 +0100

    Update the tests

--- vm/setter_map_test.go
+++ vm/setter_map_test.go
@@ -2,16 +2,17 @@ package vm
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/actgardner/gogen-avro/vm/generated"
-	"github.com/actgardner/gogen-avro/vm/setter"
+	"github.com/actgardner/gogen-avro/vm/setters"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 var (
-	mapInputData = []byte{
+	complexMapInputData = []byte{
 		84,
 		0, 42,
 
@@ -36,34 +37,94 @@ var (
 		0, // End of blocks
 	}
 
-	mapReaderByteCode = []byte{
+	complexMapReaderByteCode = []byte{
 		byte(OpMov), byte(TypeInt),
 		byte(OpMovOpt), 0, byte(TypeInt),
-		byte(OpLoopStart), 7,
+		byte(OpLoopStart), 3,
 		byte(OpMov), byte(TypeString), // Outer Map key
-		byte(OpMov), byte(TypeFloat), // Outer map values
+		byte(OpCall), byte(2), // Call NestedMapRecord
+		byte(OpLoopEnd),
+		byte(OpHalt),
+		byte(OpMov), byte(TypeFloat), // NestedMapRecord
 		byte(OpLoopStart), 3,
 		byte(OpMov), byte(TypeString), // Inner Map key
 		byte(OpMov), byte(TypeInt), // Inner map values
 		byte(OpLoopEnd),
+		byte(OpRet),
+	}
+
+	simpleMapInputData = []byte{
+		84,
+
+		4, // Block length: 2 items
+
+		8, 'k', 'e', 'y', '1',
+		8, 'v', 'a', 'l', '1',
+
+		8, 'k', 'e', 'y', '2',
+		8, 'v', 'a', 'l', '2',
+
+		0, // End of blocks
+	}
+
+	simpleMapReaderByteCode = []byte{
+		byte(OpMov), byte(TypeInt),
+		byte(OpLoopStart), 3,
+		byte(OpMov), byte(TypeString), // Outer Map key
+		byte(OpMov), byte(TypeString), // Outer Map value
 		byte(OpLoopEnd),
 		byte(OpHalt),
 	}
 )
 
-func TestMapSetter(t *testing.T) {
-	p, err := NewProgram(mapReaderByteCode)
+func TestSimpleMapSetter(t *testing.T) {
+	p, err := NewProgram(simpleMapReaderByteCode)
 	assert.Nil(t, err)
 
-	var obj generated.SetterMapTestRecord
-	objSetter, err := setter.NewSetterFor(&obj)
+	var obj generated.SimpleMapTestRecord
+	objSetter, err := setters.NewSetterFor(&obj)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	engine := NewEngine(p, objSetter)
-	err = engine.Run(bytes.NewBuffer(mapInputData))
-	if err != nil && err != setter.ErrSetterEOF {
+	err = engine.Run(bytes.NewBuffer(simpleMapInputData))
+	if err != nil {
+		t.Fatalf("Program failed:\n%s\n\nFailure: %v", p.String(), err)
+	}
+
+	t.Logf("Result: %+v\n", obj)
+
+	assert.Equal(t, int32(42), obj.AInt)
+
+	require.Equal(t, 2, len(obj.Nodes))
+
+	var value generated.Nested
+	var ok bool
+
+	value, ok = obj.Nodes["key1"]
+	assert.True(t, ok)
+	assert.Equal(t, "val1", value.Val)
+
+	value, ok = obj.Nodes["key2"]
+	assert.True(t, ok)
+	assert.Equal(t, "val2", value.Val)
+}
+
+func TestMapSetter(t *testing.T) {
+	p, err := NewProgram(complexMapReaderByteCode)
+	assert.Nil(t, err)
+
+	var obj generated.SetterMapTestRecord
+	objSetter, err := setters.NewSetterFor(&obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	engine := NewEngine(p, objSetter)
+	err = engine.Run(bytes.NewBuffer(complexMapInputData))
+	fmt.Printf("Obj:\t%+v\n", obj)
+	if err != nil {
 		t.Fatalf("Program failed:\n%s\n\nFailure: %v", p.String(), err)
 	}
 
@@ -89,4 +150,20 @@ func TestMapSetter(t *testing.T) {
 	assert.Equal(t, int32(5), r.Numbers["cinco"])
 	assert.Equal(t, int32(6), r.Numbers["seis"])
 	assert.Equal(t, int32(7), r.Numbers["siete"])
+}
+
+var mainErr error
+
+func BenchmarkMapSetter(b *testing.B) {
+	var err error
+	for n := 0; n < b.N; n++ {
+		p, _ := NewProgram(complexMapReaderByteCode)
+
+		var obj generated.SetterMapTestRecord
+		objSetter, _ := setters.NewSetterFor(&obj)
+
+		engine := NewEngine(p, objSetter)
+		err = engine.Run(bytes.NewBuffer(complexMapInputData))
+	}
+	mainErr = err
 }
--- vm/setter_test.go
+++ vm/setter_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/actgardner/gogen-avro/vm/generated"
-	"github.com/actgardner/gogen-avro/vm/setter"
+	"github.com/actgardner/gogen-avro/vm/setters"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -21,19 +21,19 @@ var (
 		// Node :: Name
 		20, 70, 105, 114, 115, 116, 32, 110, 111, 100, 101,
 
-		// Node :: Address (valid)
+		// *Node :: Address (valid)
 		2,
+
 		2, // Id
-
-		// Next :: Address (valid)
+		// *Next :: Address (valid)
 		2,
+
 		4, // Id
-
-		// Next :: Address (valid)
+		// * Next :: Address (valid)
 		2,
-		6, // Id
 
-		// Next :: Address (nil)
+		6, // Id
+		// *Next :: Address (nil)
 		0,
 
 		// OptAddr :: Address (valid)
@@ -49,17 +49,63 @@ var (
 		byte(OpCall), 4,
 		byte(OpLoad),
 		byte(OpJmpEq), 0, 1,
-		byte(OpCall), 6,
+		byte(OpCall), 8,
 		byte(OpHalt),
-		byte(OpMov), byte(TypeString), // 4: Node reader
+		// Node reader
+		byte(OpMov), byte(TypeString),
 		byte(OpLoad),
-		byte(OpJmpEq), 0, 1,
+		byte(OpJmpEq), 1, 2,
+		byte(OpSkip),
+		byte(OpRet),
 		byte(OpCall), 1,
 		byte(OpRet),
-		byte(OpMov), byte(TypeInt), // Address reader
+		// Address reader
+		byte(OpMov), byte(TypeInt),
 		byte(OpLoad),
-		byte(OpJmpEq), 0, 1,
-		byte(OpJmp), -4 & 0xff,
+		byte(OpJmpEq), 0, 2,
+		byte(OpCall), -4 & 0xff,
+		byte(OpRet),
+		byte(OpSkip),
+		byte(OpRet),
+	}
+
+	reorderedInputData = []byte{
+		// Node :: Name
+		20, 70, 105, 114, 115, 116, 32, 110, 111, 100, 101,
+
+		// Node :: Address (valid)
+		2,
+		2, // Id
+
+		// Next :: Address (nil)
+		0,
+
+		84, // AInt
+	}
+
+	reorderedReaderByteCode = []byte{
+		byte(OpSort), 2, 3, 0,
+		byte(OpCall), 5,
+		byte(OpMov), byte(TypeInt),
+		byte(OpSkip),
+		byte(OpSkip),
+		byte(OpSkip),
+		byte(OpHalt),
+		// Node reader
+		byte(OpMov), byte(TypeString),
+		byte(OpLoad),
+		byte(OpJmpEq), 1, 2,
+		byte(OpSkip),
+		byte(OpRet),
+		byte(OpCall), 1,
+		byte(OpRet),
+		// Address reader
+		byte(OpMov), byte(TypeInt),
+		byte(OpLoad),
+		byte(OpJmpEq), 0, 2,
+		byte(OpCall), -4 & 0xff,
+		byte(OpRet),
+		byte(OpSkip),
 		byte(OpRet),
 	}
 )
@@ -69,7 +115,7 @@ func TestSetter(t *testing.T) {
 	assert.Nil(t, err)
 
 	var obj generated.SetterTestRecord
-	objSetter, err := setter.NewSetterFor(&obj)
+	objSetter, err := setters.NewSetterFor(&obj)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -97,12 +143,35 @@ func TestSetter(t *testing.T) {
 	require.NotNil(t, obj.Node.Addr.Next.Next)
 	assert.Equal(t, int32(3), obj.Node.Addr.Next.Next.Id)
 
-	require.NotNil(t, obj.Node.Addr.Next.Next.Next)
-	assert.Equal(t, int32(4), obj.Node.Addr.Next.Next.Next.Id)
-
-	assert.Nil(t, obj.Node.Addr.Next.Next.Next.Next)
+	assert.Nil(t, obj.Node.Addr.Next.Next.Next)
 
 	require.NotNil(t, obj.OptAddr)
-	assert.Equal(t, int32(5), obj.OptAddr.Id)
+	assert.Equal(t, int32(4), obj.OptAddr.Id)
 	assert.Nil(t, obj.OptAddr.Next)
+}
+
+func TestReorderedSetter(t *testing.T) {
+	p, err := NewProgram(reorderedReaderByteCode)
+	assert.Nil(t, err)
+
+	var obj generated.SetterTestRecord
+	objSetter, err := setters.NewSetterFor(&obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	engine := NewEngine(p, objSetter)
+	err = engine.Run(bytes.NewBuffer(reorderedInputData))
+	if err != nil {
+		t.Fatalf("Program failed:\n%s\n\nFailure: %v", p.String(), err)
+	}
+
+	assert.Equal(t, int32(42), obj.AInt)
+	assert.Nil(t, obj.OptInt)
+	assert.Nil(t, obj.NilInt)
+	assert.Equal(t, "First node", obj.Node.Name)
+	require.NotNil(t, obj.Node.Addr)
+	assert.Equal(t, int32(1), obj.Node.Addr.Id)
+	assert.Nil(t, obj.Node.Addr.Next)
+	assert.Nil(t, obj.OptAddr)
 }
