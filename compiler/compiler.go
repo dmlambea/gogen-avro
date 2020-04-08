c6982e4da910bc919f6408d6a74d4a3621dc8197
Daniel M. Lambea <dmlambea@gmail.com>
2020-04-08 23:26:50 +0100

    Add support for unions and arrays. Fix primitive types and allow registering error codes during compilation.

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -18,16 +18,22 @@ type compiler struct {
 	errors  []string
 }
 
-func (c compiler) compileType(m *method, wrt, rdr schema.GenericType) (err error) {
-	assertNotReference(wrt, "writer")
-	assertNotReference(rdr, "reader")
-
-	/* TODO
-	// If the writer is not a union but the reader is, try and find the first matching type in the union as a target
-	if !isUnionType(wrt) && isUnionType(rdr) {
-		return c.compileNonUnionToUnion(wrt, rdr.(*schema.UnionType))
+func (c compiler) getOrCreateError(msg string) int {
+	for i := range c.errors {
+		if c.errors[i] == msg {
+			return i
+		}
 	}
-	*/
+	c.errors = append(c.errors, msg)
+	return len(c.errors) - 1
+}
+
+func (c compiler) compileType(m *method, wrt, rdr schema.GenericType) (err error) {
+	// If the writer is not a union/optional type but the reader is, try and find
+	// the first matching type in the reader as a target.
+	if rdr != nil && !isUnionType(wrt) && isUnionType(rdr) {
+		return c.compileNonUnionToUnion(m, wrt, rdr.(*schema.UnionType))
+	}
 
 	switch wrtType := wrt.(type) {
 	case *schema.RecordType:
@@ -52,7 +58,7 @@ func (c compiler) compileType(m *method, wrt, rdr schema.GenericType) (err error
 	case *schema.EnumType:
 		panic("enum types are not implemented yet")
 	case *schema.UnionType:
-		panic("union types are not implemented yet")
+		return c.compileUnion(m, wrtType, rdr)
 	case *schema.MapType:
 		rdrType, ok := rdr.(*schema.MapType)
 		if !ok && rdr != nil {
@@ -64,7 +70,15 @@ func (c compiler) compileType(m *method, wrt, rdr schema.GenericType) (err error
 		}
 		return c.compileBlock(m, wrtType, rdrType)
 	case *schema.ArrayType:
-		panic("array types are not implemented yet")
+		rdrType, ok := rdr.(*schema.ArrayType)
+		if !ok && rdr != nil {
+			return fmt.Errorf("incompatible types %s and %s", wrt.Name(), rdr.Name())
+		}
+		// Beware the nil interfaces!
+		if rdr == nil {
+			return c.compileBlock(m, wrtType, nil)
+		}
+		return c.compileBlock(m, wrtType, rdrType)
 	case schema.GenericType:
 		return c.compilePrimitive(m, wrt, rdr)
 	}
@@ -72,6 +86,9 @@ func (c compiler) compileType(m *method, wrt, rdr schema.GenericType) (err error
 }
 
 func (c compiler) compilePrimitive(m *method, wrt, rdr schema.GenericType) error {
+	if rdr != nil && !wrt.IsReadableBy(rdr, make(schema.VisitMap)) {
+		return fmt.Errorf("incompatible types %s and %s", wrt.Name(), rdr.Name())
+	}
 	t, err := vmTypeFor(wrt)
 	switch {
 	case err != nil:
@@ -105,6 +122,7 @@ func (c compiler) compileBlock(m *method, wrt, rdr schema.GenericType) (err erro
 	if rdr != nil {
 		rdrChildType = rdr.(schema.SingleChildType).Type()
 	}
+
 	if err = c.compileType(m, wrtChildType, rdrChildType); err != nil {
 		return
 	}
@@ -122,7 +140,21 @@ func (c compiler) compileBlock(m *method, wrt, rdr schema.GenericType) (err erro
 }
 
 func (c compiler) compileRecord(m *method, wrt, rdr *schema.RecordType) (err error) {
-	order, allAsc, err := getReadOrder(wrt, rdr)
+	var rdrFields []schema.GenericType
+	if rdr != nil {
+		rdrFields = rdr.Children()
+	}
+	order, allAsc, err := getReadOrder(wrt.Children(), rdrFields, func(field *schema.FieldType) (rdrField *schema.FieldType, err error) {
+		if rdr != nil {
+			// All children within a record are fields, and must be found by name matching the given one
+			if rdrField = rdr.FindFieldByNameOrAlias(field); rdrField != nil {
+				if !field.Type().IsReadableBy(rdrField.Type(), make(schema.VisitMap)) {
+					err = fmt.Errorf("incompatible schemas: field %s in reader has incompatible type in writer field %s", rdrField.Name(), field.Name())
+				}
+			}
+		}
+		return
+	})
 	if err != nil {
 		return err
 	}
@@ -172,6 +204,99 @@ func (c compiler) compileRecord(m *method, wrt, rdr *schema.RecordType) (err err
 	return nil
 }
 
+func (c compiler) compileUnion(m *method, wrt *schema.UnionType, rdr schema.GenericType) (err error) {
+	var rdrFields []schema.GenericType
+	if rdr != nil {
+		if ct, ok := rdr.(schema.CompositeType); ok {
+			rdrFields = ct.Children()
+		} else {
+			rdrFields = append(rdrFields, schema.NewField("non-union-field", rdr, 0))
+		}
+	}
+	order, allAsc, err := getReadOrder(wrt.Children(), rdrFields, func(field *schema.FieldType) (rdrField *schema.FieldType, err error) {
+		for i := range rdrFields {
+			candidate := rdrFields[i].(*schema.FieldType)
+			if field.Type().IsReadableBy(candidate.Type(), make(schema.VisitMap)) {
+				if candidate.Type().Name() == field.Type().Name() {
+					return candidate, nil
+				}
+				// The best option found up to date
+				rdrField = candidate
+			}
+		}
+		if rdrField == nil {
+			err = fmt.Errorf("incompatible schemas: reader has no compatible fields for writer field %s in union", field.Name())
+		}
+		return
+	})
+	if err != nil {
+		return err
+	}
+
+	if !allAsc {
+		// Must invoke Sort for rearranging union fields
+		refinedOrder := refineOrder(order)
+		m.append(vm.Sort(refinedOrder))
+	}
+
+	m.append(vm.Load())
+	skipJmpPos := -1
+	if wrt.IsOptional() {
+		// This jmp's rel offset has to be corrected later
+		skipJmpPos = m.Size()
+		m.append(vm.JmpEq(int64(wrt.OptionalIndex()), 0))
+	}
+	m.append(vm.Mov(vm.TypeAcc))
+
+	sw := newSwitch()
+	for i := range order {
+		if i == wrt.OptionalIndex() && wrt.IsOptional() {
+			continue
+		}
+		wrtField := asField(wrt.Children()[i])
+		switch order[i] {
+		case oDiscardable:
+			fmt.Printf("Panic: discarding wrtField %d, that is a %s\n", wrtField.Index(), wrtField.Type().Name())
+			panic("this should never happen in a union")
+		case oSkippable:
+			fmt.Printf("should this ever happen in a union?? I'm skipping wrtField %d, that is a %s\n", wrtField.Index(), wrtField.Type().Name())
+			m.append(vm.Skip())
+		default:
+			typeMethod := newMethod("")
+			var rType schema.GenericType
+			if rdr != nil {
+				f := asField(rdrFields[order[i]])
+				rType = f.Type()
+			}
+			err = c.compileType(typeMethod, wrtField.Type(), rType)
+			if err != nil {
+				return
+			}
+			sw.addCase(int64(wrtField.Index()), typeMethod)
+		}
+	}
+	sw.compileTo(m, c.getOrCreateError("invalid index for union"))
+	if skipJmpPos > -1 {
+		// Fix the optional jmp from the beginning
+		jmpPos := m.Size() - skipJmpPos - 1
+		m.code[skipJmpPos] = vm.JmpEq(int64(wrt.OptionalIndex()), jmpPos)
+	}
+	return nil
+}
+
+// compileNonUnionToUnion tries to find the first matching field in the reader
+// for the giver writer type.
+func (c compiler) compileNonUnionToUnion(m *method, wrt schema.GenericType, rdr *schema.UnionType) (err error) {
+	for _, child := range rdr.Children() {
+		childType := child.(*schema.FieldType).Type()
+		if !wrt.IsReadableBy(childType, make(schema.VisitMap)) {
+			continue
+		}
+		return c.compileType(m, wrt, childType)
+	}
+	return fmt.Errorf("incompatible types %s and %s", wrt.Name(), rdr.Name())
+}
+
 // getOrCreateMethodFor returns an existing method for the given type,
 // or creates a new one, if not exists. The returning bool value will be
 // true if the method has been created anew.
@@ -193,7 +318,7 @@ func (c *compiler) getOrCreateMethodFor(w, r schema.GenericType) (*method, bool)
 
 func (c compiler) discard(m *method, t schema.GenericType) error {
 	switch t.(type) {
-	case *schema.RecordType, *schema.MapType:
+	case *schema.RecordType, *schema.MapType, *schema.ArrayType:
 		return c.compileType(m, t, nil)
 	default:
 		ft, err := vmTypeFor(t)
