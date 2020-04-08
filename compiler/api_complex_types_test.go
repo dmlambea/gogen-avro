730d28373fec8922ecd51c5f70f42246f133a2fe
Daniel M. Lambea <dmlambea@gmail.com>
2020-04-08 23:28:15 +0100

    Move complex types testing to golden files

--- compiler/api_complex_types_test.go
+++ compiler/api_complex_types_test.go
@@ -7,151 +7,76 @@ import (
 
 	"github.com/actgardner/gogen-avro/vm"
 	"github.com/actgardner/gogen-avro/vm/setters"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+type complexTypeObjectSetterPair struct {
+	object interface{}
+	setter setters.Setter
+}
+
+type complexTypeRoundtripFixture struct {
+	index     int
+	name      string
+	wrtSchema []byte
+	inputData []byte
+	target    complexTypeObjectSetterPair
+}
+
+type complexTypeEnumIntRecString struct {
+	Enum int32
+	AInt int32
+	ARec struct {
+		InnerInt int32
+	}
+	Text string
+}
+
+type complexTypeFloatRecsliceStringRecBytes struct {
+	AFloat    float32
+	ARecArray []struct {
+		InnerInt int32
+	}
+	AString string
+	ARec    struct {
+		InnerInt int32
+	}
+	ABytes []byte
+}
+
 func TestCompileComplex(t *testing.T) {
-	wrtSchema := `{
-			"type": "record",
-			"name": "TestRecord",
-			"fields": [ {
-				"name": "aBytes",
-				"type": "bytes"
-			}, {
-				"name": "aRecord",
-				"type": {
-					"name": "SimpleRecord",
-					"type": "record",
-					"fields": [
-						{
-							"name": "innerInt",
-							"type": "int"
-						}
-					]
-				}
-			}, {
-				"name": "aString",
-				"type": "string"
-			}, {
-				"name": "aFloat",
-				"type": "float"
-			}, {
-				"name": "aInt",
-				"type": "int"
-			}, {
-				"name": "aIntMap",
-				"type": {"type": "map", "values": "int"}
-			} ]
-		}`
-	wrtData := []byte{
-		8, 0, 1, 2, 3, // bytes: {0, 1, 2, 3}
-		84,               // SimpleRecord::innerInt: 42
-		6, 'H', 'i', '!', // string: 'Hi!'
-		0, 1, 2, 3, // float
-		42, // int: 21
-		4,
-		6, 'O', 'n', 'e',
-		2,
-		6, 'T', 'w', 'o',
-		4,
-		0,
-	}
-
-	rdrSchemas := []string{
-		/*
-			`{
-				"type": "record",
-				"name": "TestRecord",
-				"fields": [ {
-					"name": "aBool",
-					"type": "bool"
-				}, {
-					"name": "aRecord",
-					"type": {
-						"name": "SimpleRecord",
-						"type": "record",
-						"fields": [
-							{
-								"name": "innerInt",
-								"type": "int"
-							}
-						]
-					}
-				}, {
-					"name": "aString",
-					"type": "string"
-				}, {
-					"name": "aEnum",
-					"type": "enum",
-					"values": ["A", "B", "C"]
-				}, {
-					"name": "aInt",
-					"type": "int"
-				}, {
-					"name": "aIntMap",
-					"type": {"type": "map", "values": "int"}
-				} ]
-			}`,
-			`{
-				"type": "record",
-				"name": "TestRecord",
-				"fields": [ {
-					"name": "aInt",
-					"type": "int"
-				}, {
-					"name": "aBool",
-					"type": "bool"
-				}, {
-					"name": "aRecord",
-					"type": {
-						"name": "SimpleRecord",
-						"type": "record",
-						"fields": [
-							{
-								"name": "innerInt",
-								"type": "int"
-							}
-						]
-					}
-				}, {
-					"name": "aIntMap",
-					"type": {"type": "map", "values": "int"}
-				}, {
-					"name": "aString",
-					"type": "string"
-				}, {
-					"name": "aEnum",
-					"type": "enum",
-					"values": ["A", "B", "C"]
-				} ]
-			}`,*/
-		`{
-			"type": "record",
-			"name": "TestRecord",
-			"fields": [ {
-				"name": "aInt",
-				"type": "int"
-			} ]
-		}`,
-	}
-
-	for i := range rdrSchemas {
-		p, err := CompileSchemaBytes([]byte(wrtSchema), []byte(rdrSchemas[i]))
+	setterOrFail := func(s setters.Setter, err error) setters.Setter {
 		require.Nil(t, err)
-		fmt.Printf("Program:\n%s\n", p)
-
-		var obj struct {
-			Placeholder int32
-		}
-		objSetter, err := setters.NewSetterFor(&obj)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		engine := vm.NewEngine(p, objSetter)
-		err = engine.Run(bytes.NewBuffer(wrtData))
-		require.Nil(t, err)
-		assert.Equal(t, int32(21), obj.Placeholder)
+		return s
 	}
+
+	var obj1 complexTypeEnumIntRecString
+	var obj2 complexTypeFloatRecsliceStringRecBytes
+	pairs := []complexTypeObjectSetterPair{
+		{object: &obj1, setter: setterOrFail(setters.NewSetterFor(&obj1))},
+		{object: &obj2, setter: setterOrFail(setters.NewSetterFor(&obj2))},
+	}
+
+	f := complexTypeRoundtripFixture{name: "complex_struct"}
+	f.wrtSchema = goldenLoad(f.name + "_writer.avsc")
+	f.inputData = goldenLoad(f.name + "_data.bin")
+
+	for i := range pairs {
+		f.index = i
+		f.target = pairs[i]
+		roundtrip(t, f)
+	}
+}
+
+func roundtrip(t *testing.T, f complexTypeRoundtripFixture) {
+	readerSchema := goldenLoad(fmt.Sprintf("%s_reader_%d.avsc", f.name, f.index))
+
+	prog, err := CompileSchemaBytes(f.wrtSchema, readerSchema)
+	require.Nil(t, err)
+	goldenEquals(t, fmt.Sprintf("%s_%d.asm", f.name, f.index), []byte(prog.String()))
+
+	engine := vm.NewEngine(prog, f.target.setter)
+	err = engine.Run(bytes.NewBuffer(f.inputData))
+	require.Nil(t, err)
+	goldenEquals(t, fmt.Sprintf("%s_%d.out", f.name, f.index), []byte(fmt.Sprintf("%+v", f.target.object)))
 }
