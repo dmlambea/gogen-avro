f7dd7efc84e15c5c3cac2cdf3e17989a235d68c7
Daniel M. Lambea <dmlambea@gmail.com>
2020-04-10 20:34:51 +0100

    Fix tests

--- compiler/api_complex_types_test.go
+++ compiler/api_complex_types_test.go
@@ -6,21 +6,15 @@ import (
 	"testing"
 
 	"github.com/actgardner/gogen-avro/vm"
-	"github.com/actgardner/gogen-avro/vm/setters"
 	"github.com/stretchr/testify/require"
 )
-
-type complexTypeObjectSetterPair struct {
-	object interface{}
-	setter setters.Setter
-}
 
 type complexTypeRoundtripFixture struct {
 	index     int
 	name      string
 	wrtSchema []byte
 	inputData []byte
-	target    complexTypeObjectSetterPair
+	target    interface{}
 }
 
 type complexTypeEnumIntRecString struct {
@@ -45,25 +39,20 @@ type complexTypeFloatRecsliceStringRecBytes struct {
 }
 
 func TestCompileComplex(t *testing.T) {
-	setterOrFail := func(s setters.Setter, err error) setters.Setter {
-		require.Nil(t, err)
-		return s
-	}
-
 	var obj1 complexTypeEnumIntRecString
 	var obj2 complexTypeFloatRecsliceStringRecBytes
-	pairs := []complexTypeObjectSetterPair{
-		{object: &obj1, setter: setterOrFail(setters.NewSetterFor(&obj1))},
-		{object: &obj2, setter: setterOrFail(setters.NewSetterFor(&obj2))},
+	targets := []interface{}{
+		&obj1,
+		&obj2,
 	}
 
 	f := complexTypeRoundtripFixture{name: "complex_struct"}
 	f.wrtSchema = goldenLoad(f.name + "_writer.avsc")
 	f.inputData = goldenLoad(f.name + "_data.bin")
 
-	for i := range pairs {
+	for i := range targets {
 		f.index = i
-		f.target = pairs[i]
+		f.target = targets[i]
 		roundtrip(t, f)
 	}
 }
@@ -71,12 +60,15 @@ func TestCompileComplex(t *testing.T) {
 func roundtrip(t *testing.T, f complexTypeRoundtripFixture) {
 	readerSchema := goldenLoad(fmt.Sprintf("%s_reader_%d.avsc", f.name, f.index))
 
-	prog, err := CompileSchemaBytes(f.wrtSchema, readerSchema)
+	p, err := CompileSchemaBytes(f.wrtSchema, readerSchema)
 	require.Nil(t, err)
-	goldenEquals(t, fmt.Sprintf("%s_%d.asm", f.name, f.index), []byte(prog.String()))
+	goldenEquals(t, fmt.Sprintf("%s_%d.asm", f.name, f.index), []byte(p.String()))
 
-	engine := vm.NewEngine(prog, f.target.setter)
-	err = engine.Run(bytes.NewBuffer(f.inputData))
+	engine := vm.Engine{
+		Program:     p,
+		StackTraces: true,
+	}
+	err = engine.Run(bytes.NewBuffer(f.inputData), f.target)
 	require.Nil(t, err)
-	goldenEquals(t, fmt.Sprintf("%s_%d.out", f.name, f.index), []byte(fmt.Sprintf("%+v", f.target.object)))
+	goldenEquals(t, fmt.Sprintf("%s_%d.out", f.name, f.index), []byte(fmt.Sprintf("%+v", f.target)))
 }
